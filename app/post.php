a8d4003116ca825baa9bc7510b85868acee3f175
darulfajarr <151610191@gmail.com>
2018-02-06 13:04:57 +0700

    crud pertama

---
+++ app/post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    protected $table = 'posts';
+    protected $fillable = ['title','content'];
+
+    public $timestamps = true;
+}
